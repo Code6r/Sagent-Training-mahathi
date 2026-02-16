65c1a7f40b3b4ee4027d7b0ad0743e29ef4a53c8
Mahathi <146954429+Code6r@users.noreply.github.com>
2026-02-16 11:41:27 +0530

    Delete Spring-Boot Implementation directory

--- Spring-Boot Implementation/groceryapp/src/main/java/com/example/groceryapp/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.groceryapp.repository;
-
-import com.example.groceryapp.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
